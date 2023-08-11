028893907d5d76adfca49ef465a6ee6dc561213e
yk11 <kothiyayogesh11@gmail.com>
2023-08-11 17:49:25 +0530

    Remove Console

--- apollo/apollo.ts
+++ apollo/apollo.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-console.log(process.env.NEXTAPI_URL, "process.env.NEXTAPI_URL");
+
 const client = new ApolloClient({
-  uri:  process.env.NEXTAPI_URL || 'https://contractor-demo-nikunjpatel190.vercel.app/api/graphql',// 'http://localhost:3001/api/graphql', // Change this URL to your GraphQL API endpoint
+  uri:  process.env.NEXTAPI_URL || 'https://contractor-demo-nikunjpatel190.vercel.app/api/graphql',
+  // uri: 'http://localhost:3001/api/graphql',
   cache: new InMemoryCache(),
 });
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,96 +18,6 @@ const HELLO_QUERY = gql`
   }
 `;
 
-
-/* const getData = async ( resHandle: any) => {
-  console.log("----")
-  let dataRs = {};
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-
-  const graphql = JSON.stringify({
-    query: "{\n  contractors {\n    id\n    name\n    specialities\n    availability\n    day_rate\n    color\n  }\n}",
-    variables: {}
-  });
-
-
-  let reData = await fetch("http://localhost:4000/graphql", {
-    method: 'POST',
-    headers: myHeaders,
-    body: graphql,
-    redirect: 'follow'
-  });
-
-  if (reData.status == 200) {
-    dataRs = await reData.json();
-  }
-
-  console.log(dataRs?.data?.contractors, "dataRs?.data?.contractors")
-
-  resHandle(dataRs?.data?.contractors)
-  
-
-} */
-
-/* const rowResData = [
-    
-  {
-    "id": "1",
-    "name": "Strykes",
-    "specialities": [
-      "Surgery",
-      "Medical"
-    ],
-    "availability": "Yes",
-    "day_rate": "$ 300,00",
-    "color": "#3F893E"
-  },
-  {
-    "id": "2",
-    "name": "Rossonerri",
-    "specialities": [
-      "Healthcare",
-      "Fitness"
-    ],
-    "availability": "No",
-    "day_rate": "$ 250,00",
-    "color": "#3E4589"
-  },
-  {
-    "id": "3",
-    "name": "Tyga",
-    "specialities": [
-      "Surgery",
-      "Medical"
-    ],
-    "availability": "Yes",
-    "day_rate": "$ 199,00",
-    "color": "#883E89"
-  },
-  {
-    "id": "4",
-    "name": "Yolo.corp",
-    "specialities": [
-      "Automotive",
-      "Modification"
-    ],
-    "availability": "Yes",
-    "day_rate": "$ 200,00",
-    "color": "#89863E"
-  },
-  {
-    "id": "5",
-    "name": "Bardi",
-    "specialities": [
-      "Technology",
-      "Home Living"
-    ],
-    "availability": "No",
-    "day_rate": "$ 100,00",
-    "color": "#893E3E"
-  }
-]; */
-
 function Home() {
   const { loading, error, data } = useQuery(HELLO_QUERY);
   /* if (loading) return <p>Loading...</p>;
@@ -153,8 +63,6 @@ function Home() {
   const handleFilterModel = () => {
     setFilterModel(!filterModel);
   }
-
-  // React.useCallback(() => getData((resDt:any)=>setData(resDt)), []);
 
   return (
     <Layout>
